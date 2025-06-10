0deccb4dd0623472c6e57219b035b477bff5103c
kjuiop <copying_y@naver.com>
2025-06-11 08:41:36 +0900

    feat: config load func add

--- echo-server/Server/src/main/java/com/gig/echoserver/EchoServer.java
+++ echo-server/Server/src/main/java/com/gig/echoserver/EchoServer.java
@@ -2,8 +2,18 @@ package com.gig.echoserver;
 
 public class EchoServer {
 
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        System.out.println("Starting server on port " + port);
+    }
+
     public static void main(String []args) {
         int port = Config.getInt("server.port", 8080);
-        System.out.println("server port: " + port);
+        new EchoServer(port).start();
     }
 }
